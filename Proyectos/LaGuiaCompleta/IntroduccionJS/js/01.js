cc03e7624de80d9728d3ffbfa5b0324a776c1799
JefersonRomero <j.daniel0320@gmail.com>
2024-03-31 23:52:44 -0500

    video 13 Manipulation of Objects

--- Proyectos/LaGuiaCompleta/IntroduccionJS/js/01.js
+++ Proyectos/LaGuiaCompleta/IntroduccionJS/js/01.js
@@ -1,13 +1,13 @@
 // Repaso JS
 
 // const no se puede reasignar y debe arrancar con un valor
-const nombre = 'Jeferson';
+const nombreJ = 'Jeferson';
 
 // let se puee reasignar y no requiere un valor inicial
 let equipo = 'Chelsea';
 
 console.log('Hello Work');
-console.log(`El equipo favorito de ${nombre} es ${equipo}`);
+console.log(`El equipo favorito de ${nombreJ} es ${equipo}`);
 
 let cliente;
 // Para conocer el tipo de dato, usamos typeof
@@ -22,10 +22,66 @@ const descuento = null;
 console.log(`valor nulo: ${descuento}`);
 
 // valores undefines
-const precio = null;
-console.log(`valor undefine: ${precio}`);
+const precios = null;
+console.log(`valor undefine: ${precios}`);
 
 // Symbol, nunca seran iguales
 const primerSymbol = Symbol(30);
 const segundoSymbol = Symbol(30);
 console.log(`Comparacion entre Symbol: ${primerSymbol === segundoSymbol}`);
+
+// Objetos
+console.log('---------- Objetos Inicio ----------')
+const producto = {
+    nombre : 'Tablet',
+    precio : 300,
+    disponible : false
+}
+
+console.log(producto)
+// Otra forma de representarlo
+console.table(producto)
+// para acceder a una sola propiedad o atributo
+console.log(`El Objeto producto tiene el atributo nombre con el valor de "${producto.nombre}"`)
+console.log('---------- Aplicando Destructuring ----------')
+const { nombre, precio, disponible } = producto
+console.log(`El nombre es ${nombre}`)
+console.log(`El precio es ${precio}`)
+console.log(`La disponibilidad es ${disponible}`)
+
+// Esto seria igual a esto, pero el anterior Script es la forma moderna:
+// const nombre = producto.nombre
+// const precio = producto.precio
+// const disponible = producto.disponible
+// console.log(`El nombre es ${nombre}`)
+// console.log(`El precio es ${precio}`)
+// console.log(`La disponibilidad es ${disponible}`)
+console.log('---------- Object Literal Enhacement ----------')
+// agrupar variables en un objeto
+// si el key y el value se llaman igual, se puede aplicar como en el atributo de autenticado, de lo contrario tocaria usar como esta en usuario1
+const autenticado = true
+const usuario1 = 'Marco Romero'
+const nuevoObjeto = {
+    autenticado,
+    usuario : usuario1
+}
+console.log(nuevoObjeto)
+console.log('---------- Manipulación de Objetos ----------')
+const carta = {
+    tipo : 'Oscuridad',
+    atk : 2500,
+    efecto : false
+}
+
+// Object.freeze(carta) // No permite modificar eliminar o editar ninguna propiedad
+Object.seal(carta) // No permite agregar o eliminar, pero si permite editar propiedades
+// Reescribir un valor
+carta.efecto = true
+
+// Si no existe lo agrega
+carta.img = 'magoOscuro.jpg'
+
+// Eliminar propiedad
+delete carta.atk
+console.log(carta)
+console.log('---------- Objetos Fin ----------')
--- Readme.md
+++ Readme.md
@@ -1,6 +1,9 @@
 <p align="center">
   <img src="./img/react.png" alt="https://es.react.dev/"/>
 </p>
+<p align= "center" style= "color: #149ECA">
+  <strong><span style="font-size: 30px;">CURSO REACT</span></strong>
+</p>
 
 # Resumen
 En este Proyecto se tienen ejercicios de React de los cursos que se han visto en diferentes plataformas.
